ffea911806b3b32e5bff81571bfdfc24957282d4
Abhishek Ezhava <abhishek.ezhava@contentstack.com>
2023-01-29 20:29:15 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,104 @@
+import "./styles.css";
+import Plx from "react-plx";
+
+export default function App() {
+  return (
+    <div>
+      <Plx
+        parallaxData={[
+          {
+            start: 0,
+            end: 700,
+            easing: "ease-in",
+            properties: [
+              {
+                startValue: 1,
+                endValue: 1.6,
+                property: "scale"
+              }
+            ]
+          }
+        ]}
+        style={{
+          position: "fixed",
+          left: 0,
+          top: 0,
+          width: "100%",
+          zIndex: 100
+        }}
+      >
+        <img style={{ width: "100%" }} src="bg.png" alt="foreground" />
+      </Plx>
+      <Plx
+        parallaxData={[
+          {
+            start: 0,
+            end: 800,
+            properties: [
+              {
+                startValue: 1,
+                endValue: 1.18,
+                property: "scale"
+              }
+            ]
+          }
+        ]}
+        style={{  
+          position: "fixed",
+          left: 0,
+          top: 0,
+          width: "100%"
+        }}
+      >
+        <img style={{ width: "100%" }} src="background.jpg" alt="background" />
+      </Plx>
+      <Plx
+        parallaxData={[
+          {
+            start: 0,
+            end: 400,
+            properties: [
+              {
+                startValue: 1,
+                endValue: 0,
+                property: "opacity"
+              }
+            ]
+          }
+        ]}
+        style={{
+          position: "fixed",
+          left: 0,
+          top: "26vw",
+          width: "100%"
+        }}
+      >
+        <img
+          style={{
+            width: "30vw"
+          }}
+          src="/text-img.webp"
+          alt="Goonies"
+        />
+      </Plx>
+      <div
+        style={{
+          position: "fixed",
+          lefft: 0,
+          top: 0,
+          zIndex: 200,
+          paddingTop: "56%",
+          height: "400vh",
+          width: "100%"
+        }}
+      >
+        <div
+          style={{
+            background: "#000",
+            height: "100%"
+          }}
+        ></div>
+      </div>
+    </div>
+  );
+}
